4c7136fd6b004780f2159555488831ee94e4b0c6
José Henrique Martins Pinheiro <pinheiro012345@gmail.com>
2022-10-17 17:15:38 -0300

    editation

--- mysite.py
+++ mysite.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
 # criar a primeira pagina do site
 # route -> link do site
 # função para exibir algo na pagina
+
+nomes = ['P.Duarte', 'Henrique', 'P.Alexandria', 'P.Elssie']
+meta = 264
+'''
+
+fig = go.Figure(data=[
+    go.line(name='Meta', x=nomes, y=[264, 264, 264, 264]),
+    go.Bar(name='Alcançado', x=nomes, y=[240, 230, 215, 210]),
+    go.Bar(name='Dias Fora', x=nomes, y=[8, 3, 0, 5]),
+    #go.Bar(name='Concluido')
+])
+# Change the bar mode
+fig.update_layout(barmode='group')
+
+grafic = fig.show()
+'''
+
 
 
 @app.route('/')
@@ -14,6 +32,10 @@ def home():
 @app.route("/mesa/<id_mesa>")
 def mesa(id_mesa):
     return render_template("mesa.html", id_mesa=id_mesa)
+
+@app.route("/grafic")
+def grafic(grafic):
+    return render_template("grafic.html", grafic=grafic)
 
 
 
